e9746b6dee5ca6daf47f0d8f46e89a2c31c768e2
abhishek tomar <abhishek.tomar@bolster.ai>
2023-10-28 00:31:46 +0530

    adding a basic layout for sign in and sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Auth } from "./components/Auth/Auth";
 
 function App() {
   return (
     <div className="App">
-      <h1>Blue Litmus App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-up" element={<Auth isSignInPage={false} />} />
+          <Route path="/sign-in" element={<Auth isSignInPage={true} />} />
+          <Route path="/" element={<Auth isSignInPage={true} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
